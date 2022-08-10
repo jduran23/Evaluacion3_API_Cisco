407839a536c5bc6498dbfdc0c500be07e52ca71a
Julio Duran <jduran23@gmail.com>
2022-08-09 22:48:21 -0400

    se agrega funcion top_system

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import requests
 import json
 import conf
 
+sandbox = "https://10.10.20.14"
 
-def obtener_token():
+def obtener_token(usuario, clave):
     url = "https://10.10.20.14/api/aaaLogin.json"
 
     body = {
         "aaaUser": {
             "attributes": {
-                "name": conf.usuario,
-                "pwd": conf.clave
+                "name": usuario,
+                "pwd": clave
             }
         }
     }
@@ -21,7 +22,20 @@ def obtener_token():
     respuesta = requests.post(url, headers=cabecera, data=json.dumps(body), verify=False)
     token = respuesta.json()['imdata'][0]['aaaLogin']['attributes']['token']
     return token
+#token = obtener_token(conf.usuario, conf.clave)
+#GET http://apic-ip-address/api/class/topSystem.json
+#top_system()
 
-token = obtener_token()
+def top_system():
 
-print(token)
+    cabecera = {
+        "Content-Type": "application/json"
+    }
+    galleta = {
+        "APIC-Cookie": obtener_token(conf.usuario, conf.clave)
+    }
+    requests.packages.urllib3.disable_warnings()
+    respuesta = requests.get(sandbox+"/api/class/topSystem.json", headers=cabecera, cookies=galleta, verify=False)
+    print(respuesta.json())
+
+top_system()
