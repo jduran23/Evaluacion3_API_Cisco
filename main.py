f0f4642303373576e939f10f5f8461670886211f
Julio Duran <jduran23@gmail.com>
2022-08-10 01:01:04 -0400

    version con funcion top_system, status_node y statusBD

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import conf
 
 sandbox = "https://10.10.20.14"
 
+
 def obtener_token(usuario, clave):
     url = "https://10.10.20.14/api/aaaLogin.json"
 
@@ -22,16 +23,15 @@ def obtener_token(usuario, clave):
     respuesta = requests.post(url, headers=cabecera, data=json.dumps(body), verify=False)
     token = respuesta.json()['imdata'][0]['aaaLogin']['attributes']['token']
     return token
-#token = obtener_token(conf.usuario, conf.clave)
-#GET http://apic-ip-address/api/class/topSystem.json
-#top_system()
-#obtener_token(conf.usuario, conf.clave)
-#GET http://apic-ip-address/api/mo/topology/pod-1/node-1/sys/ch/bslot/board/sensor-3.json
-#GET http://APIC-IP/api/mo/uni/tn-TENANT_NAME.json
-#GET URL: http://APIC-IP/api/mo/uni/tn-TENANT_NAME.json?query-target=subtree&target-subtree-class=fvBD
+
+
+# token = obtener_token(conf.usuario, conf.clave)
+# GET http://apic-ip-address/api/class/topSystem.json
+# http://APIC-IP/api/mo/topology/pod-1/node-101/sys.json
+# obtener_token(conf.usuario, conf.clave)
+# GET URL: http://APIC-IP/api/mo/uni/tn-TENANT_NAME.json?query-target=subtree&target-subtree-class=fvBD
 
 def top_system():
-
     cabecera = {
         "Content-Type": "application/json"
     }
@@ -39,7 +39,7 @@ def top_system():
         "APIC-Cookie": obtener_token(conf.usuario, conf.clave)
     }
     requests.packages.urllib3.disable_warnings()
-    respuesta = requests.get(sandbox+"/api/class/topSystem.json", headers=cabecera, cookies=galleta, verify=False)
+    respuesta = requests.get(sandbox + "/api/class/topSystem.json", headers=cabecera, cookies=galleta, verify=False)
     print(respuesta.json())
 
 
@@ -51,11 +51,11 @@ def status_node():
         "APIC-Cookie": obtener_token(conf.usuario, conf.clave)
     }
     requests.packages.urllib3.disable_warnings()
-    respuesta = requests.get(sandbox+"/api/mo/topology/pod-1/node-101/sys.json", headers=cabecera, cookies=galleta, verify=False)
+    respuesta = requests.get(sandbox + "/api/mo/topology/pod-1/node-101/sys.json", headers=cabecera, cookies=galleta,
+                             verify=False)
     print(respuesta.json())
 
 def status_bridgedomain():
-
     cabecera = {
         "Content-Type": "application/json"
     }
@@ -63,15 +63,8 @@ def status_bridgedomain():
         "APIC-Cookie": obtener_token(conf.usuario, conf.clave)
     }
     requests.packages.urllib3.disable_warnings()
-    respuesta = requests.get(sandbox+"/api/mo/uni/tn-Heroes.json?query-target=subtree&target-subtree-class=fvBD", headers=cabecera, cookies=galleta, verify=False)
+    respuesta = requests.get(sandbox + "/api/mo/uni/tn-Heroes.json?query-target=subtree&target-subtree-class=fvBD",
+                             headers=cabecera, cookies=galleta, verify=False)
     print(respuesta.json())
 
-
-
-
-
-
-
-
-
-
+status_bridgedomain()
